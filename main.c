ccad0dee648dc88c91c19e0984d0a5592fcbb74c
SOULmos <chojy465@gmail.com>
2020-06-03 20:28:22 +0900

    두 번째 커밋입니다.

--- main.c
+++ main.c
@@ -2,5 +2,20 @@
 
 int main(void) {
   printf("Hello World\n");
+  int a, b, n;
+  printf("1.+, 2.-, 3.*, 4./ select : ");
+  scanf("%d", &n);
+  printf("input a,b:");
+  scanf("%d %d", &a, &b);
+  switch(n) {
+    case 1:
+      printf("%d + %d = %d\n", a, b, a + b);
+    case 2:
+      printf("%d - %d = %d\n", a, b, a - b);
+    case 3:
+      printf("%d * %d = %d\n", a, b, a * b);
+    case 4:
+      printf("%d / %d = %d\n", a, b, a / b);
+  }
   return 0;
 }
